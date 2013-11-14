4b75d5e49be438093c93a9f34a453180d2d4df0d
anders.jaensson <anders.jaensson@smartbear.com>
2013-11-14 11:32:31 +0100

    SOAP-989: storing the access token

--- soapui/src/main/java/com/eviware/soapui/impl/wsdl/submit/filters/OauthRequestFilter.java
+++ soapui/src/main/java/com/eviware/soapui/impl/wsdl/submit/filters/OauthRequestFilter.java
@@ -6,27 +6,26 @@ import com.eviware.soapui.impl.wsdl.submit.transports.http.BaseHttpRequestTransp
 import com.eviware.soapui.model.iface.SubmitContext;
 import com.eviware.soapui.model.project.Project;
 import com.eviware.soapui.model.support.ModelSupport;
+import com.eviware.soapui.support.StringUtils;
+import com.eviware.soapui.support.UISupport;
 import oauth.signpost.OAuth;
 import oauth.signpost.OAuthConsumer;
 import oauth.signpost.OAuthProvider;
-import oauth.signpost.basic.DefaultOAuthProvider;
 import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
 import oauth.signpost.commonshttp.CommonsHttpOAuthProvider;
-import oauth.signpost.exception.OAuthCommunicationException;
-import oauth.signpost.exception.OAuthExpectationFailedException;
-import oauth.signpost.exception.OAuthMessageSignerException;
-import oauth.signpost.exception.OAuthNotAuthorizedException;
 import org.apache.http.HttpRequest;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.awt.*;
+import java.net.URI;
 
 /**
  * @author Anders Jaensson
  */
 public class OauthRequestFilter extends AbstractRequestFilter
 {
+
+	private String tokenSecret;
+	private String token;
 
 	@Override
 	public void filterRestRequest( SubmitContext context, RestRequestInterface request )
@@ -42,40 +41,29 @@ public class OauthRequestFilter extends AbstractRequestFilter
 		OAuthProvider provider = new CommonsHttpOAuthProvider(
 				"https://api.twitter.com/oauth/request_token",
 				"https://api.twitter.com/oauth/access_token",
-				"https://api.twitter.com/oauth/authorize");
+				"https://api.twitter.com/oauth/authorize" );
 
 		try
 		{
-			String authUrl = provider.retrieveRequestToken( consumer, OAuth.OUT_OF_BAND );
+			if( StringUtils.isNullOrEmpty( token ) || StringUtils.isNullOrEmpty( tokenSecret ) )
+			{
+				String authUrl = provider.retrieveRequestToken( consumer, OAuth.OUT_OF_BAND );
+				Desktop.getDesktop().browse( URI.create( authUrl ) );
+				String code = UISupport.getDialogs().prompt( "Please enter the code", "title" );
+				provider.retrieveAccessToken( consumer, code );
 
-			System.out.println("Now visit:\n" + authUrl + "\n... and grant this app authorization");
-			System.out.println("Enter the verification code and hit ENTER when you're done");
-
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			String code = br.readLine();
-			provider.retrieveAccessToken( consumer, code );
-
+				token = consumer.getToken();
+				tokenSecret = consumer.getTokenSecret();
+			}
+			else
+			{
+				consumer.setTokenWithSecret( token, tokenSecret );
+			}
 			HttpRequest httpRequest = ( HttpRequest )context.getProperty( BaseHttpRequestTransport.HTTP_METHOD );
 			// sign the request
 			consumer.sign( httpRequest );
 		}
-		catch( OAuthMessageSignerException e )
-		{
-			SoapUI.logError( e );
-		}
-		catch( OAuthExpectationFailedException e )
-		{
-			SoapUI.logError( e );
-		}
-		catch( OAuthCommunicationException e )
-		{
-			SoapUI.logError( e );
-		}
-		catch( OAuthNotAuthorizedException e )
-		{
-			SoapUI.logError( e );
-		}
-		catch( IOException e )
+		catch( Exception e )
 		{
 			SoapUI.logError( e );
 		}
